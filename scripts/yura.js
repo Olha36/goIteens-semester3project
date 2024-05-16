590669634024dbbc000fc0d0fb6b3f3a319cf3b3
yurij842shvets <yurij2011shvets@gmail.com>
2024-05-16 18:43:45 +0300

    add football layout css js and img file

--- scripts/yura.js
+++ scripts/yura.js
@@ -1,1 +1,10 @@
-console.log('Yura');
+const footBall = document.querySelector('.football-img')
+console.log(footBall);
+
+const footballField = document.querySelector('.football-field')
+console.log(footballField);
+
+function getClickCoordinates(event) {
+
+}
+footballField.addEventListener('click', getClickCoordinates)
