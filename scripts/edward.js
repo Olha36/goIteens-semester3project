369f8e1d47e71232975e303321b6e720df10f88d
Edward Dudas <edwarddudas@gmail.com>
2024-05-21 20:14:09 +0300

    push js fiel

--- scripts/edward.js
+++ scripts/edward.js
@@ -1,1 +1,14 @@
-console.log('Edward');
+const calculator = document.querySelector(".search-button-wrapper input");
+const reButton = document.querySelector(".resultButton");
+const result = document.querySelector(".result");
+let variuble = calculator.value;
+// let hoursResult = Math.floor(variuble/60);
+// let minuteResult = Math.floor(hoursResult/60);
+
+function calculation() {
+    Math.floor(variuble/60);
+    Math.floor(hoursResult/60);
+    
+}
+
+reButton.addEventListener('click', calculation);
