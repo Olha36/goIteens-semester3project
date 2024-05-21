2af96e2f868b16a57cc084b00cb9767b89449d4e
Nazar_Panyushchyk <npanusik@gmail.com>
2024-05-21 21:30:51 +0300

    add functions

--- scripts/nazar.js
+++ scripts/nazar.js
@@ -4,3 +4,23 @@ const startGameButton = document.querySelector('.startGameButton')
 const againGameButton = document.querySelector('.againGameButton')
 const dinoImg = document.querySelector('.dinoImage')
 const cactusImg = document.querySelector('.cactusImage')
+
+function startGame() {
+    console.log(startGame);
+}
+startGame()
+
+function stopGame() {
+    console.log(stopGame);
+}
+stopGame()
+
+function dinoJump() {
+    console.log(dinoJump);
+}
+dinoJump()
+
+function cactusMovement() {
+    console.log(cactusMovement);
+}
+cactusMovement()
