25178d87aacad7f23cb762f343a167924a0f4a2c
Daniil10Sh <danik2022odessa@gmail.com>
2024-05-21 21:29:03 +0300

    add imgs , html , css , js

--- scripts/danya.js
+++ scripts/danya.js
@@ -1,11 +1,20 @@
-const imgofstone = document.querySelector('.stone')
+const imgOfStone = document.querySelector('.stone')
 
-const imgofknives = document.querySelector('.khives')
+const imgOfKnives = document.querySelector('.scissors')
 
-const imgofpaper = document.querySelector('.paper')
+const imgOfPaper = document.querySelector('.paper')
  
 const computer_option = document.querySelector('.computer_option')
 
 
 
-const Stoneknivespapergame = ["камінь", "ножиці", "папір"]
+const stoneKnivesPapergame = ["камінь", "ножиці", "папір"]
+
+
+const computer = document.querySelector('.computer')
+ let computer1 = 0;
+
+
+const player = document.querySelector('.player')
+let player1 = 0;
+
