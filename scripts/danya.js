215fbfde66f04598aefcbf4f3a09f97aad34e7d2
Daniil10Sh <danik2022odessa@gmail.com>
2024-05-23 19:51:24 +0300

    ADD JS

--- scripts/danya.js
+++ scripts/danya.js
@@ -1,6 +1,6 @@
 const imgOfStone = document.querySelector('.stone')
 
-const imgOfKnives = document.querySelector('.scissors')
+const imgOfScissors = document.querySelector('.scissors')
 
 const imgOfPaper = document.querySelector('.paper')
  
@@ -18,3 +18,54 @@ const computer = document.querySelector('.computer')
 const player = document.querySelector('.player')
 let player1 = 0;
 
+
+
+
+
+
+
+
+
+
+
+
+function getComputerChoice() {
+    const randomIndex = Math.floor(Math.random() * stoneKnivesPapergame.length);
+    return stoneKnivesPapergame[randomIndex];
+}
+
+function getPlayerChoice(choice) {
+    return choice;
+}
+
+function showComputerChoice(choice) {
+    computer_option.textContent = `Комп вибрав: ${choice}`;
+}
+
+
+
+function playGame(playerChoice) {
+    const computerChoice = getComputerChoice();
+    showComputerChoice(computerChoice);
+
+    if (playerChoice === computerChoice) {
+        displayMessage('Нічия!');
+    } else if (
+        (playerChoice === "камінь" && computerChoice === "ножиці") ||
+        (playerChoice === "ножиці" && computerChoice === "папір") ||
+        (playerChoice === "папір" && computerChoice === "камінь")
+    ) {
+        displayMessage('Гравець виграв!');
+        player1++;
+        updatePlayerScore();
+    } else {
+        displayMessage('Комп виграв!');
+        computer1++;
+        updateComputerScore();
+    }
+
+}
+
+imgOfStone.addEventListener('click', () => playGame('камінь'));
+imgOfScissors.addEventListener('click', () => playGame('ножиці'));
+imgOfPaper.addEventListener('click', () => playGame('папір'));
