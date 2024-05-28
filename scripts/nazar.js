c674036858721e1d4f27b902ca264ab93cb931ce
Olha36 <olhakucheruk36@gmail.com>
2024-05-28 23:35:52 +0300

    fix layout and css

--- scripts/nazar.js
+++ scripts/nazar.js
@@ -1,58 +1,60 @@
-// const startGameButton = document.querySelector('.startGameButton')
-// const againGameButton = document.querySelector('.againGameButton')
-// const dinoImg = document.querySelector('.dinoImage')
-// const cactusImg = document.querySelector('.cactusImage')
+const startGameButton = document.querySelector('.startGameButton')
+const againGameButton = document.querySelector('.againGameButton')
+const dinoImg = document.querySelector('.dinoImage')
+const cactusImg = document.querySelector('.cactusImage')
 
-// let intervalID;
+let intervalID;
 
-// function startGame() {
-//     if(!intervalID) {
-//         intervalID = setInterval(getCoordinates, 10)
-//     }
-//     cactusRunning()
-//     console.log('running');
-// }
-// startGame()
+function startGame() {
+    if(!intervalID) {
+        intervalID = setInterval(getCoordinates, 10)
+    }
+    cactusRunning()
+    console.log('running');
+}
+startGame()
 
 
-// function stopGame() {
-//    if(intervalID) {
-//     intervalID = null;
-//    }
-//    cactusImg.classList.remove('cactusRunning')
-// }
-// stopGame()
+function stopGame() {
+   if(intervalID) {
+    intervalID = null;
+   }
+   cactusImg.classList.remove('cactusRunning')
+}
+stopGame()
 
-// function dinoJump(e) {
-//     let key = e.key;
-//     console.log(key);
-//     if(key === 'Spacebar' || key === ' ') {
-//         dinoImg.classList.add('dinoRunning')
+function dinoJump(e) {
+    let key = e.key;
+    // console.log(key);
+    if(key === 'Spacebar' || key === ' ') {
+        dinoImg.classList.add('dinoRunning')
 
-//         setTimeout(() => {
-//             dinoImg.classList.remove('dinoRunning')
-//         }, 300)
-//     }
-// }
+        setTimeout(() => {
+            dinoImg.classList.remove('dinoRunning')
+        }, 300)
+    }
+}
 
-// function cactusRunning() {
-//     cactusImg.classList.add('cactusRunning');
-// }
+function cactusRunning() {
+  if(cactusImg.classList != 'cactusRunning') {
+    cactusImg.classList.add('cactusRunning');
+  }
+}
 // cactusRunning()
 
-// function getCoordinates() {
-//     const isIntersectionX = cactusImg.left < dinoImg.right
-//     console.log(isIntersectionX);
+function getCoordinates() {
+    const dinoCoordinates = dinoImg.getBoundingClientRect()
+    const cactusCoordinates = cactusImg.getBoundingClientRect()
+    const isIntersectionX = cactusCoordinates.left < dinoCoordinates.right
+    // console.log(isIntersectionX);
 
-//     const isIntersectionY = cactusImg.top < dinoImg.bottom
-//     console.log(isIntersectionY);
+    const isIntersectionY = cactusCoordinates.top < dinoCoordinates.bottom
+    // console.log(isIntersectionY);
 
-//     const dinoCoordinates = dinoImg.getBoundingClientRect()
-//     const cactusCoordinates = cactusImg.getBoundingClientRect()
-//     console.log(dinoCoordinates);
-//     console.log(cactusCoordinates);
-// }
-// getCoordinates()
+    
+    // console.log(dinoCoordinates);
+    // console.log(cactusCoordinates);
+}
 
-// document.addEventListener('keypress', dinoJump)
-// startGameButton.addEventListener('click', startGame)
+document.addEventListener('keypress', dinoJump)
+startGameButton.addEventListener('click', startGame)
