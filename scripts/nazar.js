06ed9e4435e62fffe52c29a083e9da9aaf21e5b8
Nazar_Panyushchyk <npanusik@gmail.com>
2024-05-28 20:02:16 +0300

    add animations

--- scripts/nazar.js
+++ scripts/nazar.js
@@ -6,56 +6,50 @@ const cactusImg = document.querySelector('.cactusImage')
 let intervalID;
 
 function startGame() {
-    // console.log(startGame); //функцію саму в собі ми не викликаємо.
     if(!intervalID) {
         intervalID = setInterval(getCoordinates, 10)
     }
-
 }
 startGame()
 
 function stopGame() {
-   //  console.log(stopGame); //функцію саму в собі ми не викликаємо.
    if(intervalID) {
     intervalID = null;
    }
-
-   //тепер треба забрати в кактуса клас, за допомогою якого він рухається
+   cactusImg.classList.remove('cactusRunning')
 }
 stopGame()
 
 function dinoJump(e) {
     let key = e.key;
     console.log(key);
-    if(key === 'Spacebar' || key === ' ') { //назва кнопки не space, а  spacebar
-        dinoJump //функцію саму в собі ми не викликаємо. Тут треба додати клас css, при якому динозавр буде пригати.
+    if(key === 'Spacebar' || key === ' ') {
+        dinoImg.classList.add('dinoRunning')
 
         setTimeout(() => {
-            dinoImg.classList.remove('тут назва класу при якому динозавр пригає')
+            dinoImg.classList.remove('dinoRunning')
         }, 300)
     }
 }
 
 
-function cactusMovement() {
-    // console.log(cactusMovement); // не викликаємо функцію саму в собі! 
-    
-
+function cactusRunning() {
+    cactusImg.classList.add('cactusRunning');
 }
-cactusMovement()
+cactusRunning()
 
 function getCoordinates() {
+    const isIntersectionX = cactusImg.left < dinoImg.right
+    console.log(isIntersectionX);
+
+    const isIntersectionY = cactusImg.top < dinoImg.bottom
+    console.log(isIntersectionY);
+
     const dinoCoordinates = dinoImg.getBoundingClientRect()
     const cactusCoordinates = cactusImg.getBoundingClientRect()
     console.log(dinoCoordinates);
     console.log(cactusCoordinates);
 }
 getCoordinates()
-
-const isIntersectionX = cactusImg.left < dinoImg.right // цей код має бути в функції getCoordinates()
-console.log(isIntersectionX); // цей код має бути в функції getCoordinates()
-
-const isIntersectionY = cactusImg.top < dinoImg.bottom // цей код має бути в функції getCoordinates()
-console.log(isIntersectionY); // цей код має бути в функції getCoordinates()
 
 document.addEventListener('keypress', dinoJump)
