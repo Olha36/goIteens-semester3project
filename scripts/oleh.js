a41ebda0ebc126db375e5cab2251419defa2aad9
olehfff <olegsamilenko5@gmail.com>
2024-05-29 20:55:44 +0300

    test2

--- scripts/oleh.js
+++ scripts/oleh.js
@@ -1,1 +1,16 @@
-console.log('Oleh2');
+const buttonPlayAgain = document.querySelector(".button");
+console.log(buttonPlayAgain)
+
+const numberGess = document.querySelector(".number");
+console.log(numberGess)
+
+function getRandomNumber() {
+    return Math.round(Math.random() * (10 - 1) + 1)
+}
+getRandomNumber()
+
+function getCheckNumbers() {
+const inputWellYou = numberGess.value
+console.log(inputWellYou)
+}
+getCheckNumbers()
