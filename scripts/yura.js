177b7b94ac914ae34aaeac1be74056cc60ae978d
yurij842shvets <yurij2011shvets@gmail.com>
2024-05-30 19:59:47 +0300

    add variables rotation and distance and finish animation in js add adaptive for header and change html

--- scripts/yura.js
+++ scripts/yura.js
@@ -7,18 +7,28 @@ console.log(footballField);
 function getClickCoordinates(event) {
     let newPositionLeft = event.clientX - footballField.getBoundingClientRect().left;
     console.log(newPositionLeft);
+
     let newPositionTop = event.clientY - footballField.getBoundingClientRect().top;
     console.log(newPositionTop);
-    // ballImg.style.top = newPositionTop - 10 + 'px';
-    // ballImg.style.left = newPositionLeft - 10 + 'px';
+
     let currentPositionLeft = ballImg.offsetLeft + ballImg.width / 2
     console.log(currentPositionLeft);
-    let currentPositionTop = ballImg.offsetLeft + ballImg.height / 2
+
+    let currentPositionTop = ballImg.offsetTop + ballImg.height / 2
     console.log(currentPositionTop);
+
     let deltaX = newPositionLeft - currentPositionLeft
     console.log(deltaX);
+
     let deltaY = newPositionTop - currentPositionTop
     console.log(deltaY);
-    ballImg.style.transition = 'transform 0.5s ease'
+
+    let distance = Math.sqrt(deltaX * deltaX + deltaY * deltaY); 
+
+    let rotation = distance * 360 / (Math.PI * ballImg.width)
+
+    ballImg.style.transform = `translate(${deltaX}px, ${deltaY}px) rotate(${rotation}deg)`
+
+    ballImg.style.transition = 'transform 1s ease'
 }
 footballField.addEventListener('click', getClickCoordinates)
