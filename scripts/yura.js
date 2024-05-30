d3fa0cf2670a502ed793f82dbaa2c4f2e1cdabb5
yurij842shvets <yurij2011shvets@gmail.com>
2024-05-30 14:27:23 +0300

    add margins in css change html and css and add  varriables current positions and deltas in js

--- scripts/yura.js
+++ scripts/yura.js
@@ -9,9 +9,16 @@ function getClickCoordinates(event) {
     console.log(newPositionLeft);
     let newPositionTop = event.clientY - footballField.getBoundingClientRect().top;
     console.log(newPositionTop);
-    ballImg.style.top = newPositionTop - 10 + 'px';
-    ballImg.style.left = newPositionLeft - 10 + 'px';
+    // ballImg.style.top = newPositionTop - 10 + 'px';
+    // ballImg.style.left = newPositionLeft - 10 + 'px';
+    let currentPositionLeft = ballImg.offsetLeft + ballImg.width / 2
+    console.log(currentPositionLeft);
+    let currentPositionTop = ballImg.offsetLeft + ballImg.height / 2
+    console.log(currentPositionTop);
+    let deltaX = newPositionLeft - currentPositionLeft
+    console.log(deltaX);
+    let deltaY = newPositionTop - currentPositionTop
+    console.log(deltaY);
     ballImg.style.transition = 'transform 0.5s ease'
-    // ballImg.classList.add('move')
 }
 footballField.addEventListener('click', getClickCoordinates)
