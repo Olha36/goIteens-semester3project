25a49fec5d7008b2614a69b31c3c7751338d9c25
varc00l <ivalchuk4@gmail.com>
2024-06-04 20:36:58 +0300

    add html and js

--- scripts/vanya.js
+++ scripts/vanya.js
@@ -1,1 +1,16 @@
 console.log('Vanya');
+const InputFirstNumber = document.querySelector(".first_number")
+const ImputSecondNumber = document.querySelector(".second_number") 
+const InputThirdNumber = document.querySelector(".third_number")
+const Button = document.querySelector("button")
+const PHighestNumber = document.querySelector("p")
+function getRandomNumber() {
+    return Math.random();
+}
+document.getElementById('button').addEventListener('click', function() {
+    const input = document.getElementsByClassName(".first_number").value;
+    const input2 = document.getElementsByClassName(".second_number").value;
+    const input3 = document.getElementsByClassName(".third_number").value;
+    
+    showBiggestNumber(numbers);
+});
