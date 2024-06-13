a659c919bb1be275cd56ce45eed66f2160723566
Olha36 <olhakucheruk36@gmail.com>
2024-06-13 18:16:08 +0300

    fix js

--- scripts/vanya.js
+++ scripts/vanya.js
@@ -1,15 +1,14 @@
 const inputFirstNumber = document.querySelector(".first_number")
 const inputSecondNumber = document.querySelector(".second_number") 
 const inputThirdNumber = document.querySelector(".third_number")
-const button = document.querySelector("button")
-const highestNumber = document.querySelector(".result")
+const button = document.querySelector("#check")
+const highestNumber = document.querySelector("p .result");
 
-document.getElementById('check').addEventListener('click', function() {
+button.addEventListener('click', function() {
     let input = Number.parseInt(inputFirstNumber.value);
     let input2 = Number.parseInt(inputSecondNumber.value);
     let input3 = Number.parseInt(inputThirdNumber.value);
     const maxNumber = Math.max(input, input2, input3)
-    console.log(maxNumber);
     highestNumber.textContent = `${maxNumber}`
     inputFirstNumber.value = '';
     inputSecondNumber.value = '';
